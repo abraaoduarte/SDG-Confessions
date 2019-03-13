d11e1d5e5a1b4a08913016d6399b2247c9738422
Abraão Duarte <abraao.n.duarte@gmail.com>
2019-03-12 22:23:12 -0300

    Form

--- src/application/admin/confession/confession.controller.js
+++ src/application/admin/confession/confession.controller.js
@@ -4,8 +4,11 @@ class ConfessionController {
   static async list(req, res, next) {
     const confessions = await Confession.find({ active: true });
 
-    console.log('confessios', confessions);
     res.render('confissoes/index', {confessions: confessions});
+  }
+
+  static create(req, res, next) {
+    res.render('confissoes/create');
   }
 }
 
--- src/application/admin/confession/confession.routes.js
+++ src/application/admin/confession/confession.routes.js
@@ -4,5 +4,6 @@ import Confession from './confession.controller';
 let router = express.Router();
 
 router.get('/', Confession.list);
+router.get('/create', Confession.create);
 
 module.exports = router;
